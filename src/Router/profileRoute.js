2d0305ca48be866cee050d5aa98902ff3b309cef
joanpurba2 <joancarolus1@gmail.com>
2024-12-17 12:13:29 +0700

    update fitur

---
+++ src/Router/profileRoute.js
@@ -0,0 +1,102 @@
+import express from 'express';
+import {
+  getName,
+  getUpdate,
+  getFoto,
+  getUpdatePass,
+  getJumlah,
+  getPlants,
+} from '../Controller/Profile.js';
+import multer from 'multer';
+import bcrypt from 'bcrypt';
+import upload from '../middleware/multer.js';
+
+const router = express.Router();
+
+router.get('/user/:id', async (req, res) => {
+  const user_id = req.params.id;
+  try {
+    const result = await getName(user_id);
+
+    res
+      .status(200)
+      .json({ message: 'Data Nama berhasil didapatkan', data: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.get('/plants/:id', async (req, res) => {
+  const user_id = req.params.id;
+  try {
+    const result = await getJumlah(user_id);
+
+    res
+      .status(200)
+      .json({ message: 'jumlah tanaman berhasil didapatkan', data: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.get('/jenis/:id', async (req, res) => {
+  const user_id = req.params.id;
+  try {
+    const result = await getPlants(user_id);
+
+    res.status(200).json({
+      message: 'jumlah jenis tanaman berhasil didapatkan',
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.patch('/user/:id', async (req, res) => {
+  const id = req.params.id;
+  const { full_name, birthday, city, email } = req.body;
+  try {
+    const result = await getUpdate(full_name, birthday, city, email, id);
+
+    res.status(200).json({ message: 'Data berhasil di update', data: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.patch('/upload/:id', upload.single('file'), async (req, res) => {
+  const id = req.params.id;
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).send('Tidak ada file yang diunggah');
+  }
+
+  // Simpan informasi file ke database jika perlu
+  const foto = `${file.filename}`;
+  try {
+    const result = await getFoto(id, foto);
+
+    res.status(200).json({ message: 'Foto berhasil di update', data: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.patch('/updatepass/:id', async (req, res) => {
+  const user_id = req.params.id;
+  const { oldPass1, oldPass2, newPass } = req.body;
+
+  try {
+    const isMatch = await bcrypt.compare(oldPass2, oldPass1);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Password Lama salah!' });
+    }
+    const hashedNewPass = await bcrypt.hash(newPass, 10);
+    const result = await getUpdatePass(hashedNewPass, user_id);
+
+    res
+      .status(200)
+      .json({ message: 'Password Berhasil Di ubah', data: result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+export default router;
--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,60 +1,58 @@
 import db from '../Database/setup.js';
 import jwt from 'jsonwebtoken';
 
- export async function validateRegisterBody(req, res, next) {
-    const { fullName, email, password, city, birthDate } = req.body;
-    console.log(req.body);
-  
-    if (!fullName || !email || !password || !city || !birthDate) {
-      return res.status(400).json({ error: 'Semua Form Harus Diisi' });
-    }
-  
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ error: 'Email tidak valid' });
-    }
-    if (password.length < 6) {
-      return res.status(400).json({ error: 'Password minimal 6 karakter' });
-    }
-    const birthDateRegex = /^\d{4}-\d{2}-\d{2}$/;
-    if (!birthDate.match(birthDateRegex)) {
-      return res.status(400).json({ error: 'Format salah' });
-    }
-    try{
-        const sql = `SELECT * FROM users WHERE email = ?`;
-        const [result]= await db.query(sql, [email]);
-        if (result.length > 0) {
-            return res.status(400).json({ error: 'Email sudah terdaftar' });
-        }
-        next();
-    }catch(err){
-        console.error('Database error:', err);
-      return res.status(500).json({ error: 'ini error' });
-    }
-    
-    
+export async function validateRegisterBody(req, res, next) {
+  const { fullName, email, password } = req.body;
+  console.log(req.body);
+
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ error: 'Semua Form Harus Diisi' });
   }
-export function validateLoginBody(req, res, next) {
-    console.log(req.body);
-    const { email, password } = req.body;
-  
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Semua Form Harus Diisi' });
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: 'Email tidak valid' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ error: 'Password minimal 6 karakter' });
+  }
+  // const birthDateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  // if (!birthDate.match(birthDateRegex)) {
+  //   return res.status(400).json({ error: 'Format salah' });
+  // }
+  try {
+    const sql = `SELECT * FROM users WHERE email = ?`;
+    const [result] = await db.query(sql, [email]);
+    if (result.length > 0) {
+      return res.status(400).json({ error: 'Email sudah terdaftar' });
     }
-  
     next();
+  } catch (err) {
+    console.error('Database error:', err);
+    return res.status(500).json({ error: 'ini error' });
+  }
 }
-export function authenticateToken(req,res,next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null){
-        return res.sendStatus(401);
+export function validateLoginBody(req, res, next) {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Semua Form Harus Diisi' });
+  }
+
+  next();
+}
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
-        if(err){
-            return res.sendStatus(403);
-        }
-        req.user = user;
-        next();
-    })
+    req.user = user;
+    next();
+  });
 }
